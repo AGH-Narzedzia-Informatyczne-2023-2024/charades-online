81185ea95da5249d9253f854fe02cc8e511d2225
AdamSzL <adam@szlosarczyk.com>
2023-11-16 16:09:25 +0100

    Create a function for joining a room and fix styling

--- src/components/EnterRoomPage.tsx
+++ src/components/EnterRoomPage.tsx
@@ -7,9 +7,13 @@ interface EnterRoomPageInterface{
 function EnterRoomPage({changeStage}: EnterRoomPageInterface) {
     const [username, setUsername] = useState('')
 
+    function joinPrivateRoom() {
+        alert('Joining a private room')
+    }
+
     return (
-        <div className="flex flex-col items-center gap-3 bg-gray-800 h-screen">
-            <h1 className="text-3xl font-bold text-white py-3">
+        <div className="flex flex-col items-center bg-gray-800 h-screen">
+            <h1 className="text-3xl font-bold text-white py-3 my-5">
 			    Charades Online
 		    </h1>
             <div className="flex flex-col w-2/12 gap-2 bg-gray-700 p-3">
@@ -21,7 +25,7 @@ function EnterRoomPage({changeStage}: EnterRoomPageInterface) {
                     onChange={(event) => setUsername(event.target.value)}
                 />
                 <button className="bg-green-500 hover:bg-green-600 text-white px-2 py-1 rounded text-2xl" onClick={()=>changeStage(1)}>Play!</button>
-                <button className="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded text-xl">Create Private Room</button>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded text-xl" onClick={joinPrivateRoom}>Create Private Room</button>
             </div>
         </div>
     )
